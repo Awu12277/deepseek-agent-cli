84f7604d1dd68869d64bb3ab1c375a22c020073a
dxw <2497436726@qq.com>
2026-06-18 17:24:02 +0800

    fix: 修复引入路径错误

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,5 +2,5 @@ export { renderApp, unmountApp } from "./RenderScope.js";
 export type { RenderScopeHandle } from "./RenderScope.js";
 export { Spinner } from "./Spinner.js";
 export { StatusMessage } from "./StatusMessage.js";
-export { DskcodeSplash } fr./DskcodeSplash.jsplash.js";
+export { DskcodeSplash } from "./DskcodeSplash.js";
 export { ChatSession } from "./ChatSession.js";
